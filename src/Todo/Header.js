bb372f5f0ac518a89a680d13aa3bcad24f7e825a
manjeet <manjeet@zeroitsolutions.com>
2024-05-27 17:43:47 +0530

    changes

--- src/Todo/Header.js
+++ src/Todo/Header.js
@@ -2,11 +2,9 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <nav className="main">
-        <div className="logo">
-          <img src="./key.svg"alt="Logo" />
-        </div>
-        <div className="">To-Do</div>
+      <nav className="main1">
+       
+        <div className="main">To-Do</div>
       </nav>
     </>
   );
