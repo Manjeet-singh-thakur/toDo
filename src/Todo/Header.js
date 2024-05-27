aa9d7b67c7b60b6e2591122953d2bf46119b5d15
manjeet <manjeet@zeroitsolutions.com>
2024-05-27 18:03:00 +0530

    changes

--- src/Todo/Header.js
+++ src/Todo/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
+import image from "../bird_2.jpg";
 const Header = () => {
   return (
     <>
       <nav className="main">
         <div className="logo">
-          <img src="./key.jpeg" alt="logo" />
+          <img src={image} alt="logo" />
         </div>
         <div className="main1">To-Do</div>
       </nav>
